8bd6f78cd827340b478bd615e1e02c0a028548cb
AdrienBouchet1 <adrien.bouchet@agroparistech.fr>
2025-12-12 16:18:32 +0100

    struc

---
+++ Competition_Handler.py
@@ -0,0 +1,18 @@
+
+
+class CompetitionHandler : 
+
+
+
+    def __init__(self) : 
+
+
+        self.__import()
+    
+
+    def __import(self) :
+
+
+
+        import numpy as np 
+        import os
